ae629d804a07527b71f93a49d1578b166306d887
Karthik <karthik.maranahalli@tesco.com>
2020-04-02 20:44:26 +0530

    routes setup

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import './directory.styles.scss';
 import MenuItem from '../menu-item/menu-Item.component';
-
+import './directory.styles.scss';
 class Directory extends React.Component {
     constructor() {
         super();
@@ -12,7 +11,7 @@ class Directory extends React.Component {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
@@ -48,9 +47,12 @@ class Directory extends React.Component {
         return(
             <div className="directory-menu">
             {
-                this.state.sections.map(({title, id, imageUrl, size}) => (
-                    <MenuItem key={id}  title ={title} imageUrl = {imageUrl} size = {size}/>
-                ))
+                // this.state.sections.map(({title, id, imageUrl, size, linkUrl}) => (
+                //     <MenuItem key={id}  title ={title} imageUrl = {imageUrl} size = {size} linkUrl={linkUrl}/>
+                // ))
+                this.state.sections.map(({id, ...otherSectionProps}) => (
+                  <MenuItem key={id}  {...otherSectionProps}/>
+              ))
             }
             </div>
 
--- src/components/menu-item/menu-Item.component.jsx
+++ src/components/menu-item/menu-Item.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-Item.styles.scss';
+
 //below instead of writing pops and when calling in line 6 instead of calling props.title below is the destructred code
 //style takes an object
 //imageurl must be a string, so its a string interpolated value
 //class name size and menu item both declared with same class name using javascript line8
-const MenuItem = ({title,size,imageUrl}) => (
-    <div className={`${size} menu-item`} >
+//history.push(`${match.url} ${linkUrl}`)}> there is a space between both url that caused issue but the once spaceremoved like below line
+//history.push(`${match.url}${linkUrl}`)}> it worked fine
+const MenuItem = ({title,size,imageUrl,history,linkUrl,match}) => (
+    <div className={`${size} menu-item`} 
+            onClick={()=>history.push(`${match.url}${linkUrl}`)}>
+        
       <div className='background-image' 
       style={{
         backgroundImage:`url(${imageUrl})`}}  />
@@ -16,4 +22,4 @@ const MenuItem = ({title,size,imageUrl}) => (
   </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
